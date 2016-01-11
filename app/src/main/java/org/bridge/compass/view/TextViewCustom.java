ab10b6584ce1aba69aa367a031c394e5365965f0
BridgeGeorge <1650730996@qq.com>
2016-01-12 00:28:49 +0800

    fix comments

--- app/src/main/java/org/bridge/compass/view/TextViewCustom.java
+++ app/src/main/java/org/bridge/compass/view/TextViewCustom.java
@@ -6,32 +6,36 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+/**
+ * 自定义字体的TextView
+ */
 public class TextViewCustom extends TextView {
-	/**
-	 * 使用自定义字体
-	 * @param context
-	 */
+    /**
+     * 使用自定义字体
+     *
+     * @param context
+     */
 
-	public TextViewCustom(Context context) {
-		super(context);
-		initView(context);
-	}
+    public TextViewCustom(Context context) {
+        super(context);
+        initView(context);
+    }
 
-	public TextViewCustom(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		initView(context);
-	}
+    public TextViewCustom(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
 
-	public TextViewCustom(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		initView(context);
-	}
+    public TextViewCustom(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView(context);
+    }
 
-	private void initView(Context context) {
-		AssetManager assetMgr = context.getAssets();
-		Typeface font = Typeface.createFromAsset(assetMgr,
-				"fonts/Helvetica Neue CE 35 Thin.ttf");
-		setTypeface(font);
-	}
+    private void initView(Context context) {
+        AssetManager assetMgr = context.getAssets();
+        Typeface font = Typeface.createFromAsset(assetMgr,
+                "fonts/Helvetica Neue CE 35 Thin.ttf");
+        setTypeface(font);
+    }
 
 }
